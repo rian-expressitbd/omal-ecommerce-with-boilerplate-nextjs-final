a3d90e3c319d028de4a8285c8d597525239d4d72
Rian Ontu <rianontu13@gmail.com>
2025-06-17 17:13:35 +0600

    fix:fixed more typeerror

--- src/components/Navbar/NavbarMiddle.tsx
+++ src/components/Navbar/NavbarMiddle.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { notificationsData } from "@/config/notification.config";
-import { useSidebar } from "@/hooks/useSidebar";
 import { navbarRef } from "@/lib/refs";
 import Link from "next/link";
-import { SidebarToggler } from "../ui/molecules/sidebarToggler";
 import Logo from "../ui/atoms/logo";
 import { CartSheet } from "../ui/organisms/cart-sheet";
 import NotificationsDropdown from "../ui/molecules/notification-btn";
@@ -14,19 +12,28 @@ export interface NavbarProps {
 }
 
 export const NavbarMiddle = ({ className }: NavbarProps) => {
-  const { isDesktop } = useSidebar();
   return (
     <header
       ref={navbarRef}
       className={`${className} p-3 sm:p-4 md:p-5 flex bg-white dark:bg-gray-700 z-50 justify-between items-center transition-all duration-200`}
     >
       <div className='flex gap-2 sm:gap-3 items-center dark:text-white'>
-        {!isDesktop && <SidebarToggler />}
         <Link href={"/"}>
           <Logo />
         </Link>
       </div>
-
+      <div className='flex items-center'>
+        <input
+          placeholder='Search Products...'
+          type='text'
+          name=''
+          id=''
+          className='p-3  border hidden md:block w-[375px]'
+        />
+        <button className='text-white bg-[#7E22CE] p-3 hidden md:block'>
+          Search
+        </button>
+      </div>
       <div className='flex items-center gap-1 sm:gap-2 md:gap-3 lg:gap-4'>
         {/* <Button
           title='Cart Button'
